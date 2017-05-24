1e2ea8b40f9a7b6ee69fecdaf3a18dea12dfb055
Mustufain <abbasmustufain@gmail.com>
2017-05-24 13:09:10 +0300

    tuned hyper parameters xgboost trained model pickel

--- Model.py
+++ Model.py
@@ -1,63 +1,46 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LogisticRegression
-from sklearn import cross_validation
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
 import xgboost as xg
-from sklearn.model_selection import KFold
-from sklearn.metrics import log_loss
-from sklearn.manifold import t_sne
+from sklearn.model_selection import train_test_split
+import time
+import matplotlib.pyplot as plt
+from sklearn.model_selection import  GridSearchCV
+import cPickle
+import sys 
 
-def Xgb_Boost_Model():
+def Xgb_Boost_Model(file):
 
-    dataframe=pd.read_csv('Quora_Engineered_Features.csv')
-    dataframe.replace(np.nan,-5555555)
-    dataframe.replace(np.inf,-5555555)
+    PICKLE_FILE_PATH="XGBOOST_TRAIN"+".pkl"
+    dataframe=pd.read_csv(file)
+    dataframe= dataframe.replace(np.nan,-5555555)
+    dataframe= dataframe.replace(np.inf,-5555555)
     data = np.array(dataframe)
     target=data[:,-1]
-    Features=data[:,2:628]
+    Features=data[:,1:628]
+    		
+    #Hyper-tuned Parameters of xgboost
 
+    xg_boost=xg.XGBClassifier(learning_rate=0.01,
+                              n_estimators=251, max_depth=9,
+                              min_child_weight=1, gamma=0.2, subsample=0.8, colsample_bytree=0.8,
+                              objective='binary:logistic', nthread=4, scale_pos_weight=1,
+                              seed=27)
 
-    xg_boost=xg.XGBClassifier(learning_rate =0.1,
-             n_estimators=251,
-             max_depth=10,
-             min_child_weight=1,
-             gamma=0.5,
-             subsample=0.8,
-             colsample_bytree=0.8,
-             objective= 'binary:logistic',
-             nthread=-1,
-             scale_pos_weight=1,
-             seed=27)
+    xg_boost.fit(Features,target)
 
-    AUC=[]
-    # log=[]
-    kf = KFold(n_splits=5)    #----------> 5 fold cross validation
-    for train_index, test_index in kf.split(Features):
+    with open(PICKLE_FILE_PATH,'wb') as pickle_file:
 
-        X_train, X_test = Features[train_index], Features[test_index]
-        y_train, y_test = target[train_index], target[test_index]
-        xg_boost.fit(X_train, y_train)
-        y_pred = xg_boost.predict(X_test)
-        AUC.append(accuracy_score(y_test, y_pred))
-
-    print "Accuracy", np.mean(AUC)*100
-
-    #Cross-Validated-Accuracy 0.753708971283(5-fold cross validation) estimators=100
-
-
-   
+        cPickle.dump(xg_boost,pickle_file)	
 
 def Main():
 
-    import time
+
     print("Programe started....")
+    file=sys.argv[1] #File name 
     start_time = time.time()
-    Xgb_Boost_Model()
+    Xgb_Boost_Model(file)
     print("--- %s seconds ---" % (time.time() - start_time))
 
 Main()
