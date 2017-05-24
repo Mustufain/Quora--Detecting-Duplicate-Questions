c83e36989303fc0f9d38547417d4cd68425a07b4
Mustufain <abbasmustufain@gmail.com>
2017-05-24 13:08:31 +0300

    improved word2vec parameters

--- Quora_Features.py
+++ Quora_Features.py
@@ -14,7 +14,7 @@ import gensim
 import multiprocessing
 import string
 import logging
-
+import sys
 
 def Data_Cleaning(data):
 
@@ -87,50 +87,43 @@ def Feature_set2(corpus_raw):
                                                  axis=1)
 
 #------------------------------------------------------------------------------------------------------------------------------------#
-def words_to_vectors(fs1):
+def words_to_vectors(fs1,filename):
     # Once we have vectors
 
     # distance, simalrity, Ranking
 
 
+
     num_features = 300  # Dimensionality of vectors
     num_workers = multiprocessing.cpu_count()  # num of threads running in parallel so that it would run faster
     context_size = 5  # window size to look number of words in context of the given word
-    downsampling = 1e-3  # Any number between 0 and 1e-5 is good for this (doesnot look repeatedly at the same words)
+    downsampling = 7.5e-06  # Any number between 0 and 1e-5 is good for this (doesnot look repeatedly at the same words)
     seed = 1  # random number generator what part of the text we want to look and vectorize it
-    min_word_count = 5  # smallest set of words we want to recognize when converting into vector
+    min_word_count = 5  #  ignore all words with total frequency lower than this.
+    hs = 1  #if 1, hierarchical softmax will be used for model training.
+            # If set to 0 (default), and negative is non-zero, negative sampling will be used.
+    negative = 5 #if > 0, negative sampling will be used, the int for negative specifies how many "noise words"
+                 # should be drawn (usually between 5-20). Default is 5. If set to 0, no negative samping is used
+
 
     Quora_word2vec = gensim.models.Word2Vec(
-        sg=1,
+
+        sg=0,
         seed=1,
         workers=num_workers,
-        size=num_features,
-        window=context_size,
         min_count=min_word_count,
-        sample=downsampling
+        size=num_features,
+        window=context_size, #(5)
+        hs=hs,#(1)
+        negative = negative,  #(5)
+        sample=downsampling #(7.5e-06 )
 
-    )
+         )
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     Quora_word2vec = gensim.models.KeyedVectors.load_word2vec_format('wiki.en.vec', binary=False)
 
-    # There are for keeping track which words are missed
-
-    fs1['q1_words_not_in_vocab'] = ''
-    fs1['q2_words_not_in_vocab'] = ''
-    fs1['q1_words_in_vocab'] = ''
-    fs1['q2_words_in_vocab'] = ''
-
-    # There are just for analysis
-
-    fs1['q1textlength'] = ''
-    fs1['q2textlength'] = ''
-    fs1['q1word_in_vocab_length'] = ''
-    fs1['q2word_in_vocab_length'] = ''
-    fs1['q1word_not_in_vocab_length'] = ''
-    fs1['q2word_not_in_vocab_length'] = ''
-
-    # Distance columns
+    # Distance column
 
     fs1['euclidean'] = ''
     fs1['canberra'] = ''
@@ -155,53 +148,41 @@ def words_to_vectors(fs1):
                 'skewq1', 'skewq2', 'canberra', 'braycurtis', 'minkowski',
                 'wordmover', 'normwordmover', 'jaccard', 'cossimilarity']
 
-    Missing_Features = ['id', 'question1', 'question2', 'question1_tokens', 'question2_tokens', 'q1textlength',
-                        'q2textlength',
-                        'q1word_in_vocab_length', 'q2word_in_vocab_length', 'q1word_not_in_vocab_length',
-                        'q2word_not_in_vocab_length',
-                        'q1_words_in_vocab', 'q2_words_in_vocab', 'q1_words_not_in_vocab', 'q2_words_not_in_vocab',
-                        'is_duplicate']
+    q1vector = np.zeros(shape=(fs1.shape[0], num_features))
+    q2vector = np.zeros(shape=(fs1.shape[0], num_features))
 
     for index, row in fs1.iterrows():
-        # filter(function, iterable) is equivalent to[item for item in iterable if function(item)]
-
-
-
-
 
         # Words found in vocabulary
 
         q1_word = filter(lambda word: word in Quora_word2vec.vocab, row['question1_tokens'].split(','))
         q2_word = filter(lambda word: word in Quora_word2vec.vocab, row['question2_tokens'].split(','))
 
-        # words not found in vocabulary
 
-        missing_q1_word = filter(lambda word: word not in q1_word, row['question1_tokens'].split(','))
-        missing_q2_word = filter(lambda word: word not in q2_word, row['question2_tokens'].split(','))
 
-        # There features are just for analysis to see proportion of words missed in each pair in wikipedia corpus
-        fs1.set_value(index, 'q1textlength', len(row['question1_tokens'].split(',')))
-        fs1.set_value(index, 'q2textlength', len(row['question2_tokens'].split(',')))
-        fs1.set_value(index, 'q1word_in_vocab_length', len(q1_word))
-        fs1.set_value(index, 'q2word_in_vocab_length', len(q2_word))
-        fs1.set_value(index, 'q1word_not_in_vocab_length', len(missing_q1_word))
-        fs1.set_value(index, 'q2word_not_in_vocab_length', len(missing_q2_word))
+        q1_vector, q2_vector = get_vector(q1_word, q2_word, Quora_word2vec,num_features)
+        q1vector[index]=q1_vector
+        q2vector[index]=q2_vector
+        Distance_Functions(Quora_word2vec, fs1, index, q1_vector, q2_vector, row['question1_tokens'],row['question2_tokens'],num_features)
 
-        # Main work starts from here
-        q1_vector, q2_vector = get_vector(q1_word, q2_word, Quora_word2vec)
-        Questions_In_Vocab(fs1, row, index, q1_vector, q2_vector, q1_word, q2_word)
-        Missing_Words_In_Question_In_Vocab(fs1, missing_q1_word, missing_q2_word, index)
-        Distance_Functions(Quora_word2vec, fs1, index, q1_vector, q2_vector, row['question1_tokens'],
-                           row['question2_tokens'])
+    for i in range(0, num_features):
+        fs1['q1_vector_' + str(i)] = q1vector[:, i]
+
+    for i in range(0,num_features):
+        fs1['q2_vector_' + str(i)] = q2vector[:,i]
 
 
     Normalized_Word_Mover_Distance(fs1, Quora_word2vec)
 
+
+
+
+
     # Write into file here
-    Write_to_csv(fs1, Features, Missing_Features)
+    Write_to_csv(fs1, Features,num_features,filename)
 
-
-def get_vector(q1_word, q2_word, model):
+    
+def get_vector(q1_word, q2_word, model,num_features):
     q1_vector_word = []
     q2_vector_word = []
 
@@ -226,9 +207,8 @@ def get_vector(q1_word, q2_word, model):
         q2_vector[:] = np.nan
 
     if len(q1_word) == 0 and len(q2_word) == 0:
-        # q1_vector=np.zeros(shape=(300,1))
-        # q2_vector=np.zeros(shape=q1_vector.shape)
-        q1_vector = np.empty(shape=(300, 1))
+
+        q1_vector = np.empty(shape=(num_features, 1))
         q2_vector = np.empty(shape=q1_vector.shape)
         q1_vector[:] = np.nan
         q2_vector[:] = np.nan
@@ -245,101 +225,107 @@ def get_vector(q1_word, q2_word, model):
         q1_vector = np.array(np.sum(q1_vector_word, axis=0) / np.sqrt((np.sum(q1_vector_word, axis=0) ** 2).sum()))
         q2_vector = np.array(np.sum(q2_vector_word, axis=0) / np.sqrt((np.sum(q2_vector_word, axis=0) ** 2).sum()))
 
-    # row vectors (1,300)
+    # row vectors (1,num_features)
 
     q1_vector = q1_vector.reshape(1, q1_vector.shape[0])
 
     q2_vector = q2_vector.reshape(1, q2_vector.shape[0])
 
+    print q1_vector.shape
+    print q2_vector.shape
+
     return q1_vector, q2_vector
 
 
-def Distance_Functions(model, fs1, index, q1_vector, q2_vector, q1, q2):
+def Distance_Functions(model, fs1, index, q1_vector, q2_vector, q1, q2,num_features):
     try:
         fs1.set_value(index, 'euclidean', Euclidean_Distance(q1_vector, q2_vector))
 
     except Exception as e:
 
-        print e
+
         fs1.set_value(index, 'euclidean', np.nan)
 
     try:
         fs1.set_value(index, 'manhattan', Manhattan(q1_vector, q2_vector))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'manhattan', np.nan)
     try:
         fs1.set_value(index, 'jaccard', Jaccard(q1_vector, q2_vector))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'jaccard', np.nan)
     try:
         fs1.set_value(index, 'cossimilarity', Cosine_Similarity(q2_vector, q2_vector))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'cossimilarity', np.nan)
     try:
         fs1.set_value(index, 'canberra', Canberra(q1_vector, q2_vector))
     except Exception as e:
 
-        print e
+
         fs1.set_value(index, 'canberra', np.nan)
     try:
         fs1.set_value(index, 'braycurtis', Braycurtis(q1_vector, q2_vector))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'braycurtis', np.nan)
     try:
         fs1.set_value(index, 'minkowski', Minkowksi(q1_vector, q2_vector, 3))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'minkowski', np.nan)
     try:
         fs1.set_value(index, 'wordmover', Word_Mover_Distance(q1, q2, model))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'wordmover', np.nan)
     try:
-        fs1.set_value(index, 'kurtosisq1', Kurtosis_q1_vector(q1_vector))
+        fs1.set_value(index, 'kurtosisq1', Kurtosis_q1_vector(q1_vector,num_features))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'kurtosisq1', np.nan)
     try:
-        fs1.set_value(index, 'kurtosisq2', Kurtosis_q2_vector(q2_vector))
+        fs1.set_value(index, 'kurtosisq2', Kurtosis_q2_vector(q2_vector,num_features))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'kurtosisq2', np.nan)
     try:
-        fs1.set_value(index, 'skewq1', Skew_q1_vector(q1_vector))
+        fs1.set_value(index, 'skewq1', Skew_q1_vector(q1_vector,num_features))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'skewq1', np.nan)
     try:
-        fs1.set_value(index, 'skewq2', Skew_q2_vector(q2_vector))
+        fs1.set_value(index, 'skewq2', Skew_q2_vector(q2_vector,num_features))
     except Exception as e:
-        print e
+
         fs1.set_value(index, 'skewq2', np.nan)
 
 
     # ---------------------------------------------------------Write all features into csv file------------------------------------------#
 
 
-def Write_to_csv(fs1, Features, Missing_Features):
-    for i in range(0, 300):
+def Write_to_csv(fs1, Features,num_feaures,filename):
+
+    for i in range(0, num_feaures):
         Features.append('q1_vector_' + str(i))
 
-    for i in range(0, 300):
+    for i in range(0, num_feaures):
         Features.append('q2_vector_' + str(i))
 
-    Features.append('is_duplicate')
+    if filename == 'train.csv':
 
-    # Word 2 vec features
+        Features.append('is_duplicate')
+        fs1.to_csv("Quora_Features_" + str(filename) + ".csv", columns=Features, index=False)
 
-    fs1.to_csv("Quora_Engineered_Features.csv", columns=Features)
+    else:
 
-    # Missing words in word2vec-----> for analysis
+        fs1.to_csv("Quora_Features_"+str(filename)+".csv", columns=Features,index=False)
 
-    fs1.to_csv("missing_words_word2vec.csv", columns=Missing_Features)
+    #log parameters matching the file variant
+
 
 
 # -----------------------------------------Features of vectors------------------------------------------------------------------#-
@@ -373,23 +359,23 @@ def Jaccard(q1_vector, q2_vector):
     return spatial.distance.jaccard(q1_vector, q2_vector)
 
 
-def Kurtosis_q1_vector(q1_vector):
-    kurt_q1 = stats.kurtosis(q1_vector.reshape(300, 1))[0]
+def Kurtosis_q1_vector(q1_vector,num_features):
+    kurt_q1 = stats.kurtosis(q1_vector.reshape(num_features, 1))[0]
     return kurt_q1
 
 
-def Kurtosis_q2_vector(q2_vector):
-    kurt_q2 = stats.kurtosis(q2_vector.reshape(300, 1))[0]
+def Kurtosis_q2_vector(q2_vector,num_features):
+    kurt_q2 = stats.kurtosis(q2_vector.reshape(num_features, 1))[0]
     return kurt_q2
 
 
-def Skew_q1_vector(q1_vector):
-    skew_q1 = stats.skew(q1_vector.reshape(300, 1))[0]
+def Skew_q1_vector(q1_vector,num_features):
+    skew_q1 = stats.skew(q1_vector.reshape(num_features, 1))[0]
     return skew_q1
 
 
-def Skew_q2_vector(q2_vector):
-    skew_q2 = stats.skew(q2_vector.reshape(300, 1))[0]
+def Skew_q2_vector(q2_vector,num_features):
+    skew_q2 = stats.skew(q2_vector.reshape(num_features, 1))[0]
     return skew_q2
 
 
@@ -412,74 +398,18 @@ def Normalized_Word_Mover_Distance(fs1, model):
 
             fs1.set_value(index,'normwordmover',np.nan)
 
-    # -------------------------------------------Only for analysis which are not found in wordvec---------------------------------------#
-
-
-def Questions_In_Vocab(fs1, row, count_found, q1_vector, q2_vector, q1_word, q2_word):
-    for i in range(0, q1_vector.shape[1]):
-        fs1.set_value(count_found, 'q1_vector_' + str(i), q1_vector[:, i])
-
-    for i in range(0, q1_vector.shape[1]):
-        fs1.set_value(count_found, 'q2_vector_' + str(i), q2_vector[:, i])
-
-    if len(q1_word) > 1:
-
-        fs1.set_value(count_found, 'q1_words_in_vocab', ",".join(q1_word))
-
-    elif len(q1_word) == 1:
-
-        fs1.set_value(count_found, 'q1_words_in_vocab', q1_word[0])
-
-    elif len(q1_word) == 0:
-
-        fs1.set_value(count_found, 'q1_words_in_vocab', 0)
-
-    if len(q2_word) > 1:
-
-        fs1.set_value(count_found, 'q2_words_in_vocab', ",".join(q2_word))
-
-    elif len(q2_word) == 1:
-
-        fs1.set_value(count_found, 'q2_words_in_vocab', q2_word[0])
-
-    elif len(q2_word) == 0:
-
-        fs1.set_value(count_found, 'q2_words_in_vocab', 0)
-
-
-def Missing_Words_In_Question_In_Vocab(fs1, missing_q1_word, missing_q2_word, count_found):
-    if len(missing_q1_word) > 1:
-
-        fs1.set_value(count_found, 'q1_words_not_in_vocab', ",".join(missing_q1_word))
-
-    elif len(missing_q1_word) == 1:
-
-        fs1.set_value(count_found, 'q1_words_not_in_vocab', missing_q1_word[0])
-
-    elif len(missing_q1_word) == 0:
-
-        fs1.set_value(count_found, 'q1_words_not_in_vocab', 0)
-
-    if len(missing_q2_word) > 1:
-
-        fs1.set_value(count_found, 'q2_words_not_in_vocab', ",".join(missing_q2_word))
-
-    elif len(missing_q2_word) == 1:
-
-        fs1.set_value(count_found, 'q2_words_not_in_vocab', missing_q2_word[0])
-
-    elif len(missing_q2_word) == 0:
-
-        fs1.set_value(count_found, 'q2_words_not_in_vocab', 0)
 
 
 def Main():
+
     start_time = time.time()
-    data = 'train.csv'
+    data = sys.argv[1]                #file
     filtered_data = Data_Cleaning(data)
     Feature_set1(filtered_data)
     Feature_set2(filtered_data)
-    words_to_vectors(filtered_data)
+                   
+    words_to_vectors(filtered_data,data)
+                 
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
